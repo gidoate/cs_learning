0a730ebfa4453d4d712aabef5d288bf43b853897
gidoate <g.idoate92@gmail.com>
2025-03-05 18:55:10 +0100

    Related to project #2: assembler, parser & output

---
+++ The elements of computing systems I/Project 06 Assembler/src/main/parser.h
@@ -0,0 +1,13 @@
+// This code reads the input, gets next command and provides access to each part of the command
+
+#ifndef PARSER_H
+#define PARSER_H 
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// Function prototype
+int parser(int argc, char *argv[]);
+
+#endif // PARSER_H
