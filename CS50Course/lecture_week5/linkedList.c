b59e23cbb3a325956bcf3d8102290d306da4245c
gidoate <g.idoate92@gmail.com>
2025-04-11 14:43:00 +0200

    Linked list explanations

--- CS50Course/lecture_week5/linkedList.c
+++ CS50Course/lecture_week5/linkedList.c
@@ -4,9 +4,16 @@
 typedef struct node
 {
     int number; // Data of the node
-    struct node *next; // Pointer to the next node in the list}  
+    struct node *next; // Pointer to the next node in the list
 }node;
 
+int get_int(char *prompt) // Function prototype for get_int
+{
+    int number; // Variable to store the integer input
+    printf("%s", prompt);
+    scanf("%i", &number); // Read an integer from the user
+    return number; // Return the integer value
+} 
 
 int main(void)
 {
@@ -20,9 +27,17 @@ int main(void)
             fprintf(stderr, "Memory allocation failed\n");
             return 1;
         }
-        (*n).number = get_int("Enter a number: "); // Assign a value to the node - the operatotr *n.number could be swapped by n->number (get_int won't work, is part of CS50 library)
+        n->number = get_int("Enter a number: "); // Assign a value to the node - the operatotr *n.number could be swapped by n->number (get_int won't work, is part of CS50 library)
         n->next = list; // Point the new node to the old list. From here onwards, -> is used to access the next node in the list
-
+        list = n;                                                                         
     }
+    
+    node *ptr = list; // Pointer to traverse the list
+    while (ptr != NULL)
+    {
+        printf("%i\n", ptr->number); // Print the number in the current node
+        ptr = ptr->next; // Move to the next node in the list
+    }
+    
     
 }
