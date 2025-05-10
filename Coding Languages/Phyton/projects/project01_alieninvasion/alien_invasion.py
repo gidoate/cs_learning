14bad400386f3d4d06544215530f9aefc6bf69a6
gidoate <g.idoate92@gmail.com>
2025-05-10 10:54:58 +0200

    Alien Invasion start of project

--- Coding Languages/Phyton/projects/project01_alieninvasion/alien_invasion.py
+++ Coding Languages/Phyton/projects/project01_alieninvasion/alien_invasion.py
@@ -1,11 +1,18 @@
 import sys # Used to exit the game when the player quits
 import pygame
 
+from settings import Settings
+from ship import Ship
+
 def run_game():
     # Initiliaze the game and create the screen object.
     pygame.init()
-    screen = pygame.display.set_mode((1200, 600)) # Tuple to define the dimensions of the screen
+    ai_settings =  Settings()
+    screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height)) # Tuple to define the dimensions of the screen
     pygame.display.set_caption("Alien Invasion")
+
+    # Create a ship
+    ship = Ship(screen)
 
     # Start the main loop for the game.
     while True:
@@ -14,6 +21,10 @@ def run_game():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+
+        # Redraw the screen every pass through the loop
+        screen.fill(ai_settings.bg_colour)
+        ship.blitme()
         
         # Make the most recently drawn screen visible
         pygame.display.flip()
---
+++ Coding Languages/Phyton/projects/project01_alieninvasion/ship.py
@@ -0,0 +1,19 @@
+import pygame
+
+class Ship():
+    def __init__(self, screen): # Screen parameter is the screen where the ship will be loaded/draw
+        """Initialize the ship and set it's starting position"""
+        self.screen = screen
+
+        # Load the ship image and get its rect.
+        self.image = pygame.image.load('C:\EmbeddedDevelopment\cs_learning\Coding Languages\Phyton\projects\project01_alieninvasion\icons\ship.bmp')
+        self.rect = self.image.get_rect() # This is used to access the surface (screen) rect attribute
+        # pygame allows you to 
+        self.screen_rect = screen.get_rect()
+        
+        # Start each new ship at the bottom center of the screen.
+        self.rect.centerx = self.screen_rect.centerx
+        self.rect.bottom = self.screen_rect.bottom
+    def blitme(self):
+        """Draw the ship at its current location."""
+        self.screen.blit(self.image, self.rect)
