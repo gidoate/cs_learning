ec6ac33384c5d696d315988832218ad7cb39e5bf
gidoate <g.idoate92@gmail.com>
2025-05-28 14:30:24 +0200

    Learning log continuation

--- Courses/PythonCrashCourse/projects/learninglog/learning_logs/views.py
+++ Courses/PythonCrashCourse/projects/learninglog/learning_logs/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+
 from .models import Topic
+from .forms import TopicForm, EntryForm
 
 def index(request):
     """The home page for Learning Log."""
@@ -17,3 +21,34 @@ def topic(request, topic_id):
     entries = topic.entry_set.order_by('-date_added')
     context = {'topic': topic, 'entries': entries}
     return render(request, 'learning_logs/topic.html', context)
+
+def new_topic(request):
+    """Add a new topic."""
+    if request.method != 'POST':
+        # No data submitted; create a blank form.
+        form = TopicForm()
+    else:
+        # POST Submitted; process data
+        form = TopicForm(request.POST)
+        if form.is_valid():
+            form.save(commit=False)
+            return HttpResponseRedirect(reverse('learning_logs:topics'))
+    context = {'form':form}
+    return render(request,'learning_logs/new_topic.html', context)
+
+def new_entry(request, topic_id):
+    """Add a new entry for a particular topic."""
+    topic = Topic.objects.get(id=topic_id)
+    if request.method != 'POST':
+        # No data submitted; create a blank form.
+        form = EntryForm()
+    else:
+        # POST data submitted; process data.
+        form = EntryForm(data=request.POST)
+        if form.is_valid():
+            new_entry = form.save(commit=False)
+            new_entry.topic = topic
+            new_entry.save()
+            return HttpResponseRedirect(reverse('learning_logs:topic', args=[topic_id]))
+    context = {'topic': topic, 'form': form}
+    return render(request, 'learning_logs/new_entry.html', context)
